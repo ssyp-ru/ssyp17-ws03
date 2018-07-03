4445f2b77dff0a6d281e6475f2d90d156f39b13a
Gudov <dima@novator.su>
2018-07-03 20:55:58 +0700

    fix snake level restart

--- example/network_snake/main.cpp
+++ example/network_snake/main.cpp
@@ -366,7 +366,7 @@ public:
             msg.push_back(x);
             msg.push_back(y);
 
-            for( int i = 1; i < players.size(); i++ )
+            for( size_t i = 1; i < players.size(); i++ )
             {
                 tcp_server->send( i - 1, msg );
             }
@@ -380,18 +380,20 @@ public:
             sync_eat();
             return;
         }
-        switch( msg[0] )
-        {
-        case 0x03:
-            if( ((msg[1] * -1) == players[id+1].input.x) ||
-                ((msg[2] * -1) == players[id+1].input.y) )
+        if (!msg.empty()){
+            switch( msg[0] )
             {
+            case 0x03:
+                if( ((msg[1] * -1) == players[id+1].input.x) ||
+                    ((msg[2] * -1) == players[id+1].input.y) )
+                {
+                    break;
+                }
+                players[id+1].new_input.x = msg[1];
+                players[id+1].new_input.y = msg[2];
+                sync_input( id+1 );
                 break;
             }
-            players[id+1].new_input.x = msg[1];
-            players[id+1].new_input.y = msg[2];
-            sync_input( id+1 );
-            break;
         }
     }
 
@@ -499,7 +501,7 @@ public:
                             {
                                 tcp_server->send(j-1,msg);
                             }
-                            return;
+                            break;
                         }
                     }
                     restart_game();
