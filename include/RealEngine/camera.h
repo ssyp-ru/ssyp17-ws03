a89a053e3d1e157629e9248d4e52dabb6639de0f
Gudov <dima@novator.su>
2018-07-03 14:36:38 +0700

    add metr converter in camera

--- include/RealEngine/camera.h
+++ include/RealEngine/camera.h
@@ -12,6 +12,8 @@ public:
     void scale( float coff );
     re::Point2f world_to_screen( re::Point2f pos );
     re::Point2f screen_to_world( re::Point2f pos );
+    float metr_to_screen( float unit );
+    float screen_to_metr( float unit );
 private:
     re::Point2f cam_pos;
     float coff;
--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -17,7 +17,21 @@ re::Point2f Camera::world_to_screen( re::Point2f pos ) {
 }
 
 re::Point2f Camera::screen_to_world( re::Point2f pos ) {
+    //world 0,0
+    //camer 10, 10
+    //coffe 2
+    // in   10,10
     pos *= this->coff;
     pos += this->cam_pos;
     return pos;
+}
+
+float Camera::metr_to_screen( float unit )
+{
+    return unit / this->coff;
+}
+
+float Camera::screen_to_metr( float unit )
+{
+    return unit * this->coff;
 }
