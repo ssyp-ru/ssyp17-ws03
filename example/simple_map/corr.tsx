93b7063f0175741a7231c833b10a544c4c026ee2
Klimov Nikolay <klimov.ns@mail.ru>
2018-07-06 23:30:45 +0300

    Delete some examples

--- example/simple_map/corr.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="corr" tilewidth="128" tileheight="128" tilecount="1" columns="1">
- <image source="mapimg/corr.png" width="128" height="128"/>
-</tileset>
